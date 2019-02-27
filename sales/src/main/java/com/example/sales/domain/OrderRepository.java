35553748e1920ad4a933405799facd6b107dae99
Brian Kwak <koalagon@gmail.com>
2019-02-26 18:39:28 -0700

    sales/users 프로젝트 console application으로 변경

--- sales/src/main/java/com/example/sales/domain/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sales.domain;
-
-import com.example.sales.domain.OrderAggregate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<OrderAggregate, String> {
-}
--- ui/src/main/java/com/example/ui/AxonConfig.java
+++ ui/src/main/java/com/example/ui/AxonConfig.java
@@ -9,8 +9,9 @@ import org.springframework.context.annotation.Primary;
 public class AxonConfig {
     @Bean
     @Primary
-    public AxonServerConfiguration axonServerConfig() {
+    public AxonServerConfiguration axonServerConfig(AxonServerConfiguration axonServerConfiguration) {
         // Axon Server가 localhost가 아닌 경우 설정을 변경해 주어야 합니다.
-        return AxonServerConfiguration.builder().servers("127.0.0.1").build();
+        axonServerConfiguration.setServers("127.0.0.1");
+        return axonServerConfiguration;
     }
 }
